81ee54e4b55f9a3633338e632ab30af284f1c9ec
AnGgIt88 <jarbull87@gmail.com>
2021-10-19 19:07:00 +0700

    rosy: bringups for palladium

--- palladium.mk
+++ palladium.mk
@@ -9,18 +9,30 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_n_mr1.mk)
 
-# Inherit some common ProjectSakura stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common palladium stuff
+$(call inherit-product, vendor/palladium/config/common_full_phone.mk)
 
 # Inherit from rosy device
 $(call inherit-product, device/xiaomi/rosy/device.mk)
 
 # Device identifier. This must come after all inclusions
-PRODUCT_NAME := lineage_rosy
+PRODUCT_NAME := palladium_rosy
 PRODUCT_DEVICE := rosy
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi 5
 PRODUCT_MANUFACTURER := Xiaomi
+
+# Palladium Flag
+PALLADIUM_BUILD_TYPE := CARROT
+
+# PALLADIUM-PROPS For About Phone
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.palladiumdevice.maintainer=FinixDev™ \
+    ro.palladiumdevice.cpu=Adreno 506 \
+    ro.palladiumdevice.display=5.7 \
+    ro.palladiumdevice.displaytype=IPS LCD \
+    ro.palladiumdevice.camera=13MP + 5MP Front Flash Camera \
+    ro.palladiumdevice.battery=3300mAh
 
 # Boot animation
 TARGET_BOOT_ANIMATION_RES := 720
