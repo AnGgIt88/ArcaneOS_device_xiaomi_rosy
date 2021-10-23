7f72f87a71adffd79f8db5a1ce441546334ef4d2
Finix-Projetcs <jarbull87@gmail.com>
2021-10-23 12:22:59 +0700

    Update palladium_rosy.mk

--- palladium_rosy.mk
+++ palladium_rosy.mk
@@ -9,24 +9,18 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_n_mr1.mk)
 
-# Inherit some common palladium stuff
-$(call inherit-product, vendor/palladium/config/common_full_phone.mk)
+# Inherit some common arcanaos stuff
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Inherit from rosy device
 $(call inherit-product, device/xiaomi/rosy/device.mk)
 
 # Device identifier. This must come after all inclusions
-PRODUCT_NAME := palladium_rosy
+PRODUCT_NAME := aosp_rosy
 PRODUCT_DEVICE := rosy
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi 5
 PRODUCT_MANUFACTURER := Xiaomi
-
-# Palladium Flag
-PALLADIUM_BUILD_TYPE := CARROT
-
-# Palladium faceunlock
-TARGET_FACE_UNLOCK_SUPPORTED := true
 
 # Boot animation
 TARGET_BOOT_ANIMATION_RES := 720
