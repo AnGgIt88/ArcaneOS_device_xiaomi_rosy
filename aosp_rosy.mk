d91b6371bd96229da4d8809a5cb8e48b79c5766a
Finix-Projetcs <jarbull87@gmail.com>
2021-10-23 21:06:09 +0700

    Add ArcanaOS stuff

--- aosp_rosy.mk
+++ aosp_rosy.mk
@@ -11,6 +11,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_n_mr1.mk
 
 # Inherit some common arcanaos stuff
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+ARCANE_DEVICE := rosy
+ARCANE_MAINTAINER := FinixDev™
+ARCANE_GAPPED := true
 
 # Inherit from rosy device
 $(call inherit-product, device/xiaomi/rosy/device.mk)
